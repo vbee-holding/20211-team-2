1e27c24da7db7fa9d1a6657651126e2b1aef1175
20183481-anhpd <93386621+20183481-anhpd@users.noreply.github.com>
2022-02-04 09:55:22 +0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// Components
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home';
+
+// Routes
+import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="app-container">
+        <Header />
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
