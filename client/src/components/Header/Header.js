eaf0a29beb3eaeac6ac70824b2105df04fe83527
20183481-anhpd <93386621+20183481-anhpd@users.noreply.github.com>
2022-02-06 21:59:50 +0700

    add onClick event

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 
 export default function Header() {
+
+  const [inputValue, setInputValue] = useState('')
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>): void => {
+    // 'keypress' event misbehaves on mobile so we track 'Enter' key via 'keydown' event
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation();
+      onSubmit();
+    }
+  }
+
+  const onSubmit = () => window.location.href="/tim-kiem/"+inputValue;
+
   return (
     <header>
       <div id="search-bar">
@@ -12,8 +26,16 @@ export default function Header() {
               type="text"
               placeholder="Nhập nội dung tìm kiếm"
               className="search-input"
+              value={inputValue}
+              onKeyDown={onKeyDown}
+              onInput={e => setInputValue(e.target.value)}
             />
-            <input type="button" value="Tìm kiếm" className="search-button" />
+            <input 
+              type="button" 
+              value="Tìm kiếm" 
+              className="search-button" 
+              onClick={onSubmit}
+            />
           </form>
           <a href="#" className="link gray-border">Join</a>
           <a href="#" className="link join-log-btn">Log in</a>
