51938718561112a70749b63a3c532ffcefc95e86
20183481-anhpd <93386621+20183481-anhpd@users.noreply.github.com>
2022-02-08 21:40:45 +0700

    update userroute

--- client/src/App.js
+++ client/src/App.js
@@ -6,41 +6,42 @@ import { useLocation } from 'react-router-dom'
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import NavBar from './components/NavBar/NavBar';
-import Home from './components/Home/Home';
+import Home from './components/Home';
 import Search from './components/Search/Search';
-import Admin from './components/Admin'
+import Admin from './components/Admin';
+import UserRoutes from './routes/UserRoutes';
 
 // Routes
-import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const MergeRoute = () => {
-  if (useLocation().pathname.includes("/admin")) {
-    return (
-      <Routes>
-        <Route path="/admin/*" element={<Admin/>} />
-      </Routes>
-    );
-  } else {
-    return (
-      <div id="app-container">
-        <Header />
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/tim-kiem/:query" element={<Search/>} />
-
-        </Routes>
-        <Footer />
-      </div>
-    );
-  }
-}
 
 function App() {
+
+  const MergeRoutes = () => {
+    if (useLocation().pathname.includes("/admin")) {
+      return (
+        <Routes>
+          <Route path="/admin/*" element={<Admin/>} />
+        </Routes>
+      );
+    } else {
+      return (
+        <div id="app-container">
+          <Header />
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/tim-kiem/:query" element={<Search/>}/>
+            <Route path="/chu-de/*" element={<UserRoutes/>}/>
+          </Routes>
+          <Footer />
+        </div>
+      );
+    }
+  }
+
   return (
-    <Router>
-      <MergeRoute />
-    </Router>
+    <MergeRoutes />
   );
 }
 
